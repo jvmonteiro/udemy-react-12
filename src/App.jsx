1da1a140ce67101e00e9710f3254f2344e2e037a
João Vítor Monteiro <jvmonteirorj@gmail.com>
2020-07-20 10:54:49 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework"
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite JS library among engineers."
+  },
+  {
+    title: "How to use React?",
+    content: "You use React by creating components."
+  }
+];
+
+const options = [
+  {
+    label: "The color red",
+    value: "red"
+  },
+  {
+    label: "The color green",
+    value: "green"
+  },
+  {
+    label: "A shade of blue",
+    value: "blue"
+  }
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div className="App">
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Convert.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+/* Can't continue due to Trasnlate API being paid */
+const Convert = ({ language, text }) => {
+  useEffect(() => {}, [language, text]);
+
+  return (
+    <div>
+      <div> Convert</div>
+    </div>
+  );
+};
+
+export default Convert;
